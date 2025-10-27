34e313330c1c03b6d1d01f98abc09394966b16e2
Baidu <baidunano.account@proton.me>
2025-10-27 08:08:08 +0700

    player update

--- scripts/components/entity/player/animation/animation.gd
+++ scripts/components/entity/player/animation/animation.gd
@@ -1,15 +1,16 @@
 extends Node2D
 
 @onready var _state := %State
-@onready var _anim := $AnimationPlayer
+@onready var _anim := $AnimationTree
 @onready var _model := $AnimatedSprite2D
+@onready var _state_machine: AnimationNodeStateMachinePlayback = _anim["parameters/playback"] if _anim != null else null
 
 func _physics_process(_delta: float) -> void:
 	match _state.current_state:
 		_state.STATE.IDLE:
-			_anim.play("idle")
+			_state_machine.travel("idle")
 		_state.STATE.RUN:
-			_anim.play("run")
+			_state_machine.travel("run")
 	match _state.current_facing:
 		_state.FACING.LEFT:
 			_model.flip_h = true
