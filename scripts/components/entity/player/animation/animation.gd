dcc54ebdf82aa8d4986db6b42709d5b5e3439c6b
Baidu <baidunano.account@proton.me>
2025-10-27 09:12:52 +0700

    new anim

--- scripts/components/entity/player/animation/animation.gd
+++ scripts/components/entity/player/animation/animation.gd
@@ -3,7 +3,13 @@ extends Node2D
 @onready var _state := %State
 @onready var _anim := $AnimationTree
 @onready var _model := $AnimatedSprite2D
-@onready var _state_machine: AnimationNodeStateMachinePlayback = _anim["parameters/playback"] if _anim != null else null
+@onready var _state_machine: AnimationNodeStateMachinePlayback = _anim["parameters/StateMachine/playback"]
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("Slowdown"):
+		_anim.set("parameters/TimeScale/scale", 0.8)
+	elif event.is_action_released("Slowdown"):
+		_anim.set("parameters/TimeScale/scale", 1.2)
 
 func _physics_process(_delta: float) -> void:
 	match _state.current_state:
